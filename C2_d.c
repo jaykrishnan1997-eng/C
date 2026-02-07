01ee75d3f9234afe5e6553d8c935ccd6f9f88872
Your Name <you@example.com>
2026-02-07 13:47:05 +0100

    all these commits have different functions for printing string

---
+++ C2_d.c
@@ -0,0 +1,10 @@
+// Program to print name using fputs("name", stdout): used for file output.  redirect this function to output using "stdout"
+
+#include <stdio.h>
+
+int main() {
+
+	fputs("Jay\n", stdout);
+
+	return 0;
+}
---
+++ C2_e.c
@@ -0,0 +1,10 @@
+//Print name using fprintf()
+
+#include <stdio.h>
+
+int main() {
+
+	fprintf(stdout, "Jay\n");
+
+	return 0;
+}
