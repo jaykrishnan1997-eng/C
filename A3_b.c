4fc9bda7509f236e11e506f91adbe3c16e78d6c9
Your Name <you@example.com>
2026-02-08 14:05:26 +0100

    program to input an integer and then display it

---
+++ A3_b.c
@@ -0,0 +1,17 @@
+// program to accept an intger from user and display it in output
+#include <stdio.h>
+
+int main() {
+	
+	//declare variable
+	int num;
+	
+	//inputing the number
+	printf("Enter the number:\n");
+	scanf("%d", &num);
+	
+	//displaying the number
+	printf("Entered number is : %d\n", num);
+
+	return 0;
+}
