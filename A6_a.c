1700cf14cad0622d0c96dd79eeebbf93ca6ce39d
Your Name <you@example.com>
2026-02-11 23:06:01 +0100

    Program with basic calculation operations

---
+++ A6_a.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+
+float add(float a, float b)
+{return a+b;}
+
+float subtract(float a, float b)
+{return a-b;}
+
+float multiply(float a, float b)
+{return a*b;}
+
+float divide(float a, float b)
+{return a/b;}
+
+//float modulo(float a, float b)
+//{return a%b;}
+
+int main()
+{
+float a,b;
+char o;
+
+printf("Enter required operator: a/A for addition, s/S for subtraction, m/M for multiplication, d/D for division, r/R for reminder");
+// %c for character, also leave a blank space character before % to ignore leftover newline.ie, " %c"
+scanf(" %c", &o);
+
+printf("Enter the two numbers:");
+// %c for character
+scanf("%f %f", &a, &b);
+//'a' is character and "a" is string, %.3f is float and include to only 3 decimal place after decimal point
+if (o == 'a' || o == 'A')
+{printf("Sum of %f and %f is : %.3f\n",a,b,add(a,b));
+return 0;}
+
+else if (o == 's' || o == 'S') 
+{printf("difference of %f and %f is : %.3f\n",a,b,subtract(a,b));
+return 0;}
+
+else if (o == 'm' || o == 'M') 
+{printf("Product of %f and %f is : %.3f\n",a,b,multiply(a,b));
+return 0;}
+
+else if (o == 'd' || o == 'D')
+  {if (b == 0)
+    {printf("Not Defined"); return 0;}
+  else 
+    {printf("Quotient of %f and %f is : %.3f\n",a,b,divide(a,b));
+    return 0;}}
+
+else if (o == 'r' || o == 'R') 
+  {if (b == 0)
+    {printf("Not Defined"); return 0;}
+  else 
+    {printf("%f modulo %f is : %.3f\n",a,b, fmod(a,b));
+    return 0;}}
+
+else
+  {printf("Wrong operator or error while choosing numbers");
+  return 0;}
+}
+
