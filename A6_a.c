ff02ab4cfdd245ac71ab6e7563ce1ee8ec559794
Your Name <you@example.com>
2026-02-11 23:34:10 +0100

    Added some next line operator

--- A6_a.c
+++ A6_a.c
@@ -19,14 +19,14 @@ int main()
 float a,b;
 char o;
 
-printf("Enter required operator: a/A for addition, s/S for subtraction, m/M for multiplication, d/D for division, r/R for reminder");
+printf("Enter required operator: a/A for addition, s/S for subtraction, m/M for multiplication, d/D for division, r/R for reminder:\n");
 // %c for character, also leave a blank space character before % to ignore leftover newline.ie, " %c"
 scanf(" %c", &o);
 
-printf("Enter the two numbers:");
+printf("Enter the two numbers:\n");
 //scanf tells you how many values it successfully match the required input. Here its 2 float 
 if (scanf("%f %f", &a, &b) != 2)
-  {printf("Invalid input"); return 1;}
+  {printf("Invalid input\n"); return 1;}
 
 
 //'a' is character and "a" is string, %.3f is float and include to only 3 decimal place after decimal point
@@ -44,20 +44,20 @@ return 0;}
 //Include part to display "Not Defines if denominator is 0"
 else if (o == 'd' || o == 'D')
   {if (b == 0)
-    {printf("Not Defined"); return 0;}
+    {printf("Not Defined\n"); return 0;}
   else 
     {printf("Quotient of %f and %f is : %.3f\n",a,b,divide(a,b));
     return 0;}}
 
 else if (o == 'r' || o == 'R') 
   {if (b == 0)
-    {printf("Not Defined"); return 0;}
+    {printf("Not Defined\n"); return 0;}
   else 
     {printf("%f modulo %f is : %.3f\n",a,b, fmod(a,b));
     return 0;}}
 // Easy way to show error for wrong input. eg wrong operator choosen
 else
-  {printf("Invalid Input");
+  {printf("Invalid Input\n");
   return 1;}
 }
 
