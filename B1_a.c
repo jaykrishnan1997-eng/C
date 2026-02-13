af68680eca28a673e9fd25cd321e5f6b033b078a
Your Name <you@example.com>
2026-02-13 18:00:33 +0100

    Added comments

--- B1_a.c
+++ B1_a.c
@@ -1,13 +1,16 @@
+//Program to check if a number is 0, negative or positive
 #include <stdio.h>
-
+//defining the function
 void Num(int N) {
-
+// if number is 0
 	if (N == 0) {
 		printf("Number is Zero\n");
 	}
+// if number is negative
 	else if (N < 0) {
 		printf("%d is Negative\n", N);
 	}
+// if number is positive
 	else  {
                 printf("%d is Positive\n", N);
         }
@@ -16,7 +19,7 @@ void Num(int N) {
 int main() {
   int N;
   printf("Enter the number\n");
-
+// Check if the input is invalid or not.
   if (scanf("%d" , &N) != 1) {
 	printf("Invalid Input!");
 	return 1;
