e4df740531d331ab7a5ab81534adbe29aee0fe3a
Your Name <you@example.com>
2026-02-22 20:25:35 +0100

    incomplete program

---
+++ B1_b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <limit.h>
+// return_type function_name(parameters) and void => function returns nothing only an anction
+void Num(int N)
+{
+	if (N == 0)
+	{
+	  printf("Given number is 0");
+	  return;
+	}
+
+	
+
+
+
+
+
+}
